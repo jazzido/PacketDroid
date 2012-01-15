a869157caa1ade999e14224e6f47e5aa9ab66f80
Georg Lukas <georg@op-co.de>
2012-01-15 18:51:22 +0100

    remove unused demodulators

--- jni/Android.mk
+++ jni/Android.mk
@@ -12,17 +12,7 @@ LOCAL_LDFLAGS          := -Wl,-Map,xxx.map
 
 LOCAL_SRC_FILES := \
    hdlc.c \
-   pocsag.c \
    demod_afsk12.c \
-   demod_afsk24.c \
-   demod_afsk24_2.c \
-   demod_dtmf.c \
-   demod_fsk96.c \
-   demod_hapn48.c \
-   demod_poc12.c \
-   demod_poc24.c \
-   demod_poc5.c \
-   demod_zvei.c \
    costabf.c \
    costabi.c \
    multimon.c
--- jni/multimon.h
+++ jni/multimon.h
@@ -139,25 +139,11 @@ struct demod_param {
 
 /* ---------------------------------------------------------------------- */
 
-extern const struct demod_param demod_poc5;
-extern const struct demod_param demod_poc12;
-extern const struct demod_param demod_poc24;
-
 extern const struct demod_param demod_afsk1200;
-extern const struct demod_param demod_afsk2400;
-extern const struct demod_param demod_afsk2400_2;
-
-extern const struct demod_param demod_hapn4800;
-extern const struct demod_param demod_fsk9600;
-
-extern const struct demod_param demod_dtmf;
-extern const struct demod_param demod_zvei;
 
 //extern const struct demod_param demod_scope;
 
-#define ALL_DEMOD &demod_poc5, &demod_poc12, &demod_poc24, \
-&demod_afsk1200, &demod_afsk2400, &demod_afsk2400_2, &demod_hapn4800, \
-    &demod_fsk9600, &demod_dtmf, &demod_zvei/*, &demod_scope*/
+#define ALL_DEMOD &demod_afsk1200
 
 
 
